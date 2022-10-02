a825b53894a6921afff9f6e04021b569d8d3298b
Назаров Михаил <85027066+movetron@users.noreply.github.com>
2022-10-02 18:24:13 +0300

    Add files via upload

--- project_№1/project_№1/Program.cs
+++ project_№1/project_№1/Program.cs
@@ -3,14 +3,16 @@ public class MainClass
 {
 
     public static void Main(string[] args) {
-
-        string a3 = Console.ReadLine(), a2 = Console.ReadLine(), a1 = Console.ReadLine(), b2 = Console.ReadLine(), b1 = Console.ReadLine();
-        string first = a3 + a2 + a1;
-        string second = b2 + b1;
-        string result = Convert.ToString(Convert.ToInt32(first) + Convert.ToInt32(second));
-        char[] number = result.ToCharArray();
-        for (int i = 0; i < number.Length; i++)
-            Console.Write(number[i] + " ");
-
+        double a, b;
+        Console.WriteLine("Введите A");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите B");
+        b = Convert.ToInt32(Console.ReadLine());
+        if (b == 0)
+            Console.WriteLine("Корень = {0}", -b / a);
+        else if (b > 0)
+            Console.WriteLine("Корни = {0}", (b / a, -b / a));
+        else
+            Console.WriteLine("Нет решений");
     }
 }

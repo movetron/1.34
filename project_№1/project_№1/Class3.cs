391d41bd864722704474d39937d802a5b31586b0
Назаров Михаил <85027066+movetron@users.noreply.github.com>
2022-10-02 18:36:48 +0300

    Add files via upload

---
+++ project_№1/project_№1/Class3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        string a3 = Console.ReadLine(), a2 = Console.ReadLine(), a1 = Console.ReadLine(), b2 = Console.ReadLine(), b1 = Console.ReadLine();
+        string first = a3 + a2 + a1;
+        string second = b2 + b1;
+        string result = Convert.ToString(Convert.ToInt32(first) + Convert.ToInt32(second));
+        char[] number = result.ToCharArray();
+        for (int i = 0; i < number.Length; i++)
+            Console.Write(number[i] + " ");
+    }
+}

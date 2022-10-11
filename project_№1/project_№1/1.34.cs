05dfaefeb16594e82f5548b90d395233eadf5931
Назаров Михаил <85027066+movetron@users.noreply.github.com>
2022-10-11 17:29:30 +0300

    Add files via upload

---
+++ project_№1/project_№1/1.34.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+ public static void Main(string[] args) {
+      double a, b;
+            Console.WriteLine("Введите A");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите B");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a != 0) { 
+
+                Console.WriteLine("Корень x = {0}", -b / a);
+
+             }
+            else { 
+                
+                Console.WriteLine("Нет решения");
+            } 
+          
+    }

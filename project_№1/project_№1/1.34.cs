019f28dada8003f5b4c3dc9e199c4f125601a36f
Назаров Михаил <85027066+movetron@users.noreply.github.com>
2022-10-31 17:26:45 +0300

    Update 1.34.cs

--- project_№1/project_№1/1.34.cs
+++ project_№1/project_№1/1.34.cs
@@ -1,21 +1,27 @@
-﻿using System;
+using System;
+public class Mainclass
+{
 
- public static void Main(string[] args) {
-      double a, b;
-            Console.WriteLine("Введите A");
-            a = Convert.ToInt32(Console.ReadLine());
+public static void Main(string[] args)
+{
+    double a, b;
+    Console.WriteLine("Введите A для линейного уравнения ax + b = 0");
+    a = Convert.ToInt32(Console.ReadLine());
+      
+    Console.WriteLine("Введите B для линейного уравнения ax + b = 0");
+    b = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Введите B");
-            b = Convert.ToInt32(Console.ReadLine());
+    if (a != 0)
+    {
 
-            if (a != 0) { 
+        Console.WriteLine("Корень x = {0}", -b / a);
 
-                Console.WriteLine("Корень x = {0}", -b / a);
-
-             }
-            else { 
-                
-                Console.WriteLine("Нет решения");
-            } 
-          
     }
+    else
+    {
+
+        Console.WriteLine("Нет решения");
+    }
+
+}
+}
